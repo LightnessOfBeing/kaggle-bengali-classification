7bab00fe6e9196a73874f296ae28c8b8eb73c699
Kirill Vishnyakov <intainer7@gmail.com>
2020-01-06 19:49:01 +0300

    removed coarse dropout

--- src/augmentations.py
+++ src/augmentations.py
@@ -7,7 +7,7 @@ def train_aug(image_size=None):
         VerticalFlip(p=0.5),
         ShiftScaleRotate(scale_limit=0.2, rotate_limit=45, shift_limit=0.15, p=0.7, border_mode=0),
       #  RandomGridShuffle(grid=(2, 2), p=0.5),
-        CoarseDropout(max_holes=10, max_height=5, max_width=5, fill_value=255, p=0.5),
+      #  CoarseDropout(max_holes=10, max_height=5, max_width=5, fill_value=255, p=0.5),
         Normalize(mean=(0.0692, 0.0692, 0.0692), std=(0.2051, 0.2051, 0.2051))]
     if image_size is not None:
         augs_list = [Resize(*image_size)] + augs_list
