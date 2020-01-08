77dcf0b2195a3130895ba369c73a1645c22833b5
Kirill Vishnyakov <intainer7@gmail.com>
2020-01-08 22:28:23 +0300

    Added gem

--- src/model.py
+++ src/model.py
@@ -1,6 +1,23 @@
+import torch
+
 import torch.nn.functional as F
 from cnn_finetune import make_model
 from torch import nn
+
+from torch.nn.parameter import Parameter
+
+def gem(x, p=3, eps=1e-6):
+    return F.avg_pool2d(x.clamp(min=eps).pow(p), (x.size(-2), x.size(-1))).pow(1./p)
+
+class GeM(nn.Module):
+    def __init__(self, p=3, eps=1e-6):
+        super(GeM,self).__init__()
+        self.p = Parameter(torch.ones(1)*p)
+        self.eps = eps
+    def forward(self, x):
+        return gem(x, p=self.p, eps=self.eps)
+    def __repr__(self):
+        return self.__class__.__name__ + '(' + 'p=' + '{:.4f}'.format(self.p.data.tolist()[0]) + ', ' + 'eps=' + str(self.eps) + ')'
 
 class MultiHeadNet(nn.Module):
     def __init__(self, arch, pretrained, num_classes):
@@ -15,6 +32,7 @@ class MultiHeadNet(nn.Module):
         self.head_grapheme_root = nn.Linear(in_features, num_classes[0])
         self.head_vowel_diacritic = nn.Linear(in_features, num_classes[1])
         self.head_consonant_diacritic = nn.Linear(in_features, num_classes[2])
+        self.gem = GeM()
 
     def freeze(self):
         for param in self.model._features.parameters():
@@ -28,7 +46,8 @@ class MultiHeadNet(nn.Module):
 
     def forward(self, x):
         features = self.model._features(x)
-        features = F.adaptive_avg_pool2d(features, 1)
+        #features = F.adaptive_avg_pool2d(features, 1)
+        features = self.gem(features)
         features = features.view(features.size(0), -1)
 
         logit_grapheme_root = self.head_grapheme_root(features)
