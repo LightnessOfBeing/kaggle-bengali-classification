5d7259138f4ebdd9a3beb4241743715cb1c2a2ed
Kirill Vishnyakov <intainer7@gmail.com>
2020-02-14 19:07:54 +0300

    Cutmix single

--- src/callbacks.py
+++ src/callbacks.py
@@ -215,6 +215,103 @@ class MixupCutmixCallback(CriterionCallback):
 
         return loss
 
+class MixupCutmixCallbackSingle(CriterionCallback):
+    def __init__(
+            self,
+            fields: List[str] = ("features",),
+            alpha=1.0,
+            on_train_only=True,
+            in_key="grapheme_root",
+            out_key="logit_grapheme_root",
+            **kwargs
+    ):
+        """
+        Args:
+            fields (List[str]): list of features which must be affected.
+            alpha (float): beta distribution a=b parameters.
+                Must be >=0. The more alpha closer to zero
+                the less effect of the mixup.
+            on_train_only (bool): Apply to train only.
+                As the mixup use the proxy inputs, the targets are also proxy.
+                We are not interested in them, are we?
+                So, if on_train_only is True, use a standard output/metric
+                for validation.
+        """
+        self.in_key = in_key
+        self.out_key = out_key
+        assert len(fields) > 0, \
+            "At least one field for MixupCallback is required"
+        assert alpha >= 0, "alpha must be>=0"
+
+        super().__init__(**kwargs)
+
+        print("Custom MixupCutmixCallback is being initialized!")
+
+        self.on_train_only = on_train_only
+        self.fields = fields
+        self.alpha = alpha
+        self.lam = 1
+        self.index = None
+        self.is_needed = True
+        self.apply_mixup = True
+
+    def on_loader_start(self, state: State):
+        self.is_needed = not self.on_train_only or \
+                         state.loader_name.startswith("train")
+
+    def do_mixup(self, state: State):
+
+        for f in self.fields:
+            state.input[f] = self.lam * state.input[f] + \
+                             (1 - self.lam) * state.input[f][self.index]
+
+    def do_cutmix(self, state: State):
+
+        bbx1, bby1, bbx2, bby2 = \
+            rand_bbox(state.input[self.fields[0]].shape, self.lam)
+
+        for f in self.fields:
+            state.input[f][:, :, bbx1:bbx2, bby1:bby2] = \
+                state.input[f][self.index, :, bbx1:bbx2, bby1:bby2]
+
+        self.lam = 1 - ((bbx2 - bbx1) * (bby2 - bby1)
+                        / (state.input[self.fields[0]].shape[-1]
+                           * state.input[self.fields[0]].shape[-2]))
+
+    def on_batch_start(self, state: State):
+        if not self.is_needed:
+            return
+
+        if self.alpha > 0:
+            self.lam = np.random.beta(self.alpha, self.alpha)
+        else:
+            self.lam = 1
+
+        self.index = torch.randperm(state.input[self.fields[0]].shape[0])
+        self.index.to(state.device)
+
+        self.apply_mixup = (np.random.rand() < 0.5)
+        if self.apply_mixup:
+            self.do_mixup(state)
+        else:
+            self.do_cutmix(state)
+
+    def _compute_loss(self, state: State, criterion):
+        loss = 0
+        if not self.is_needed:
+            pred = state.output[self.out_key]
+            y = state.input[self.in_key]
+            loss = criterion(pred, y)
+
+        else:
+            pred = state.output[self.out_key]
+            y_a = state.input[self.in_key]
+            y_b = state.input[self.in_key][self.index]
+            loss = self.lam * criterion(pred, y_a) + \
+                              (1 - self.lam) * criterion(pred, y_b)
+
+        return loss
+
 
 class CheckpointLoader(Callback):
 
