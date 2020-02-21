c002b98e59451d9adb3779c27d9913e224de2565
Kirill Vishnyakov <intainer7@gmail.com>
2020-02-22 01:47:53 +0300

    stage 2

--- src/callbacks.py
+++ src/callbacks.py
@@ -112,7 +112,7 @@ class MixupCutmixCallback(CriterionCallback):
     def __init__(
             self,
             fields: List[str] = ("features",),
-            alpha=0.4,
+            alpha=0.8,
             on_train_only=True,
             weight_grapheme_root=2.0,
             weight_vowel_diacritic=1.0,
