db813d250e5ed91b0c5debffda85181be2cafa90
Kirill Vishnyakov <intainer7@gmail.com>
2020-02-22 18:54:47 +0300

    added frn

--- src/models/efficient_new.py
+++ src/models/efficient_new.py
@@ -2,7 +2,7 @@ import timm
 from torch import nn
 
 from src.models.head import AverageHead
-from src.utils import to_Mish, to_GeM
+from src.utils import to_Mish, to_GeM, to_FRN
 
 
 class EfficientNew(nn.Module):
@@ -16,6 +16,7 @@ class EfficientNew(nn.Module):
             to_Mish(self.net)
             print("Mish activation added!")
         to_GeM(self.net)
+        to_FRN(self.net)
         print(self.net)
 
         in_features = n_channels_dict[encoder][0]
--- src/utils.py
+++ src/utils.py
@@ -7,7 +7,7 @@ import cv2
 import numpy as np
 from timm.models.activations import Swish
 from torch import nn
-from torch.nn import AdaptiveAvgPool2d, Parameter
+from torch.nn import AdaptiveAvgPool2d, Parameter, BatchNorm2d
 
 
 def load_image(path):
@@ -120,6 +120,70 @@ class GeM(nn.Module):
     def __repr__(self):
         return self.__class__.__name__ + '(' + 'p=' + '{:.4f}'.format(self.p.data.tolist()[0]) + ', ' + 'eps=' + str(
             self.eps) + ')'
+
+
+class FRN(nn.Module):
+    def __init__(self, num_features, eps=1e-6, is_eps_leanable=False):
+        """
+        weight = gamma, bias = beta
+        beta, gamma:
+            Variables of shape [1, 1, 1, C]. if TensorFlow
+            Variables of shape [1, C, 1, 1]. if PyTorch
+        eps: A scalar constant or learnable variable.
+        """
+        super(FRN, self).__init__()
+
+        self.num_features = num_features
+        self.init_eps = eps
+        self.is_eps_leanable = is_eps_leanable
+
+        self.weight = nn.parameter.Parameter(
+            torch.Tensor(1, num_features, 1, 1), requires_grad=True)
+        self.bias = nn.parameter.Parameter(
+            torch.Tensor(1, num_features, 1, 1), requires_grad=True)
+        if is_eps_leanable:
+            self.eps = nn.parameter.Parameter(torch.Tensor(1), requires_grad=True)
+        else:
+            self.register_buffer('eps', torch.Tensor([eps]))
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        nn.init.ones_(self.weight)
+        nn.init.zeros_(self.bias)
+        if self.is_eps_leanable:
+            nn.init.constant_(self.eps, self.init_eps)
+
+    def extra_repr(self):
+        return 'num_features={num_features}, eps={init_eps}'.format(**self.__dict__)
+
+    def forward(self, x):
+        """
+        0, 1, 2, 3 -> (B, H, W, C) in TensorFlow
+        0, 1, 2, 3 -> (B, C, H, W) in PyTorch
+        TensorFlow code
+            nu2 = tf.reduce_mean(tf.square(x), axis=[1, 2], keepdims=True)
+            x = x * tf.rsqrt(nu2 + tf.abs(eps))
+            # This Code include TLU function max(y, tau)
+            return tf.maximum(gamma * x + beta, tau)
+        """
+        # Compute the mean norm of activations per channel.
+        nu2 = x.pow(2).mean(dim=[2, 3], keepdim=True)
+
+        # Perform FRN.
+        x = x * torch.rsqrt(nu2 + self.eps.abs())
+
+        # Scale and Bias
+        x = self.weight * x + self.bias
+        return x
+
+
+
+def to_FRN(model):
+    for child_name, child in model.named_children():
+        if isinstance(child, BatchNorm2d):
+            setattr(model, child_name, FRN(num_features=child.num_features))
+        else:
+            to_FRN(child)
 
 
 def to_GeM(model):
