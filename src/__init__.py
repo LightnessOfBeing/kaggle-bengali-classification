15b9a1df1cf414b8dac13bfc5a9aeeac5b7269c3
Kirill Vishnyakov <intainer7@gmail.com>
2020-02-23 01:33:59 +0300

    removed callback

--- src/__init__.py
+++ src/__init__.py
@@ -22,7 +22,6 @@ registry.Callback(ImageViewerCallback)
 registry.Callback(MixupCutmixCallback)
 registry.Callback(CheckpointLoader)
 registry.Callback(HMacroAveragedRecallSingle)
-registry.Callback(MixupCutmixCallbackSingle)
 
 # Register criterion
 registry.Criterion(FocalLoss)
